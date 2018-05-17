53c481b158b8dd25835d31f664ad6367bbeb3a7c
Mateusz Kluge <inclooder@gmail.com>
2018-05-18 00:31:35 +0200

    Configure connection with neo4j

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,8 @@
 require_relative 'boot'
 
+require 'neo4j/railtie'
 require 'rails/all'
+
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
