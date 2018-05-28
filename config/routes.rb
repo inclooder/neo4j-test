cb5b4c03dee97e1d84b6f5cc01db9181cac7a343
Mateusz Kluge <inclooder@gmail.com>
2018-05-28 22:23:44 +0200

    Fix users with dots in name

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   root to: 'tweets#index'
   resource :session, only: [:new, :create, :destroy]
   resources :tweets, only: [:index, :create]
-  resources :users, only: [:show], param: :name
+  resources :users, only: [:show], param: :name, format: false, constraints: { name: /[^\/]+/ }
 end
