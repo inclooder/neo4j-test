f8331eb31b51f0650454e703ba990c5f17d2454f
Mateusz Kluge <inclooder@gmail.com>
2018-05-28 10:31:34 +0200

    Empty controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  def authenticate_user!
+    redirect_to new_session_path if session[:current_user_id].nil?
+  end
+
+  def current_user
+    @current_user ||= User.find(session[:current_user_id])
+  end
 end
---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,9 @@
+class TweetsController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+  end
+
+  def create
+  end
+end
